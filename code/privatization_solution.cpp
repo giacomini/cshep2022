b6eac4aa03c8bde7dd02062ad5ec6a3930442b6b
Francesco Giacomini <francesco.giacomini@cnaf.infn.it>
2022-12-04 14:24:23 +0100

    Add exercises about race conditions, false sharing, privatization

---
+++ code/privatization_solution.cpp
@@ -0,0 +1,65 @@
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <random>
+#include <span>
+#include <thread>
+#include <vector>
+
+auto count_5s(std::span<int> s, std::atomic<int>& count)
+{
+  for (auto&& e : s) {
+    if (e == 5) {
+      ++count;
+    }
+  }
+}
+
+auto count_5s(std::span<int> s)
+{
+  // return std::count(s.begin(), s.end(), 5);
+  auto count{0};
+  for (auto&& e : s) {
+    if (e == 5) {
+      ++count;
+    }
+  }
+  return count;
+}
+
+auto generate(int n, int n_of_5s)
+{
+  assert(n_of_5s <= n);
+  std::vector<int> v(n);
+  std::fill_n(v.begin(), n_of_5s, 5);
+  std::default_random_engine eng;
+  std::shuffle(v.begin(), v.end(), eng);
+  return v;
+}
+
+int main()
+{
+  int const n = 100'000'000;
+  std::cout << "Generating... " << std::flush;
+  auto v = generate(n, n / 4);
+  std::cout << "done\n";
+
+  auto mid_it = std::next(v.begin(), v.size() / 2);
+
+  std::vector<std::thread> vth;
+  vth.reserve(2);
+
+  std::atomic<int> count{0};
+  auto t0 = std::chrono::system_clock::now();
+
+  vth.emplace_back(std::thread{[&, s = std::span{v.begin(), mid_it}] { count += count_5s(s); }});
+  vth.emplace_back(std::thread{[&, s = std::span{mid_it, v.end()}] { count += count_5s(s); }});
+
+  for (auto&& th : vth) {
+    th.join();
+  }
+
+  auto t1 = std::chrono::system_clock::now();
+
+  std::cout << "#5 " << count << " in " << std::chrono::duration<double>{t1 - t0}.count() << " s\n";
+}
